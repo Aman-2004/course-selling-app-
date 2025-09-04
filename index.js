2d85e90ffcb4c163b3fd0dd9585889efd87c37c7
Aman-2004 <amanjha.13102001@gmail.com>
2025-09-04 20:16:46 +0530

    Step 4-Add route skeleton for user

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+const express=require("express");
+const app=express()
+app.use(express.json());
 
+app.post("/user/signup",(req,res)=>{
+    res.json({message:"This is the signup page"})
+})
+
+app.post("/user/signin",(req,res)=>{
+    res.json({message:"This is the signin page"})
+})
+
+app.post("course/purchase",(req,res)=>{
+    res.json({message:"This is to purchase a course"})
+})
+
+app.get("/courses",(req,res)=>{
+    res.json({message:"This is to see all courses"})
+})
+
+app.get("/course/purchase",(req,res)=>{
+    res.json({message:"This is to see the purchased courses"})
+})
+
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000")
+})
